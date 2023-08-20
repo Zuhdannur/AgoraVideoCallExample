b58c07a9ded6e01457286e910e5850f6543ffdb3
Zuhdan Nur <pczuhdan33@gmail.com>
2023-08-20 09:05:24 +0700

    fix build

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,4 @@
-import React, {useState} from 'react';
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import {useState} from 'react';
 import './App.css'
 import AgoraUIKit from "agora-react-uikit";
 
